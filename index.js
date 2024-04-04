270b53ade95c1e016090b19e8457bf2c4958168b
deborah <fabbydebby@gmail.com>
2024-04-04 12:41:37 +0300

    phase fetch lab

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+ return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => renderBooks(data))
+  .catch(error => console.error('Error fetching books:', error));
   
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    const h2= document.createElement('h2');
+    h2.innerHTML =book.name,
     main.appendChild(h2);
   });
 }
@@ -15,3 +20,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
